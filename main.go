6e7cdab9138574afc19abe0ea693eeaf357d22ba
Utpreksha3010 <utprekshasinghrajawat@gmail.com>
2021-10-11 22:16:11 +0530

    api

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/akansha", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	r.GET("/tik", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "tok",
+		})
+	})
+	r.Run()
+
+}
